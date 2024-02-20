0e57ab10f6e974cd511192b46d21f018a5dd6356
vincenzo8825 <vincenzorocca88@gmail.com>
2024-02-21 00:52:01 +0100

    fixx

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -12,15 +12,5 @@ export class ContactComponent implements AfterViewInit {
     AOS.init({ duration: 1200, once: true });
   }
 
-  handleSubmit(event: Event): void {
-    event.preventDefault(); // Previene il comportamento di invio di default del form
-
-    // Qui puoi aggiungere qualsiasi gestione pre invio, come controlli di validazione o manipolazione dei dati del form
-
-    // Puoi anche aggiungere qui la logica per mostrare un messaggio di conferma all'utente
-
-    // Simulare l'invio del form per Netlify se necessario, altrimenti lasciare che il form prosegua normalmente
-    // Se stai inviando il form tramite JavaScript o vuoi effettuare ulteriori azioni dopo l'invio, inseriscile qui.
-    // Altrimenti, se il form deve essere inviato direttamente a Netlify, non è necessario aggiungere altre azioni qui
-  }
+  // Nessun metodo handleSubmit necessario se stai utilizzando il comportamento di invio predefinito di Netlify
 }
